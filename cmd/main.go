f3fead23e676b2f60e3136b68581687e62a97a8f
asadbek <asadbek30665@gmail.com>
2023-02-13 19:47:08 +0500

    implement user create

--- cmd/main.go
+++ cmd/main.go
@@ -35,3 +35,8 @@ func main() {
 
 	fmt.Println(id)
 }
+
+// GetByPkey()
+// GetList()
+// Update(user)
+// Delete()
--- models/user.go
+++ models/user.go
@@ -16,6 +16,7 @@ type User struct {
 }
 
 type UpdateUser struct {
+	Id      int    `json:"id"`
 	Name    string `json:"name"`
 	Surname string `json:"urname"`
 }
